92cb1312c4890ebb58f7a902e4276f4d1caaa38b
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-09 14:11:48 +0530

    Addition of numbers

---
+++ addition.java
@@ -0,0 +1,24 @@
+/////////////////////////////
+/*class:addition
+ *Input:10
+ *      20
+ *Output:Addition is 30
+ *Description:Addition of two Number
+ *Date:9-June-2021
+ *Author Name:Shruti Nahar
+ */
+import java.util.Scanner;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter two number");
+   int n1=sc.nextInt();
+   int n2=sc.nextInt();
+   int add=n1+n2;
+   System.out.println("Addition is "+add);
+	}
+
+}
