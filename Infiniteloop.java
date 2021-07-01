f7485ed36c5ba4fef406cee98b839cfb7e2fbfa5
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-07-01 21:32:52 +0530

    Infinite Loop

---
+++ Infiniteloop.java
@@ -0,0 +1,20 @@
+/////////////////////////////
+/*Input:--
+*Output:the message Hey!!! will print infintely
+*Description:Display the Infiniteloop
+*Date:1-July-2021
+*Author Name:Shruti Nahar
+*///////////////////////////////////	
+package a;
+
+public class Infiniteloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for( ; ; )
+        {
+        	System.out.println("Hey!!!");
+        }
+	}
+
+}
