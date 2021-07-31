483adcb79d3c37872a9299c665de2dbbfae7b497
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-07-31 09:24:18 +0530

    printing the star pattern

---
+++ starpattern.java
@@ -0,0 +1,35 @@
+/////////////////////////////
+/*class:starpattern
+ *Input:--
+ *Output:
+ *  *  *  * 
+ *  *  * 
+ *  * 
+ * 
+ *Description:Printing of star pattern
+ *Date:31-July-2021
+ *Author Name:Shruti Nahar
+ *///////////////////////////////
+package practiceset4;
+
+public class starpattern {
+	public static void main(String[] args) {
+	 int n=4;
+	  
+            for(int i=n;i>0;i--)
+            {
+            	  for(int j=0;j<i;j++)
+               {
+            	   System.out.print(" * ");
+               }
+            	 
+	            System.out.print("\n");
+	            
+	        }
+       
+
+	}
+		
+}
+	
+
