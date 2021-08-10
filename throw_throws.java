5755ac4645a1ad17f093dc749f4947a6b55c7f0b
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-08-10 18:52:46 +0530

    Implementation of throw and throws keyword

---
+++ throw_throws.java
@@ -0,0 +1,47 @@
+/////////////////////////////
+/*class:throw_throws
+ *Input:--
+ *Output:--
+ *Description:Use of throw and throws keyword
+ *Date:10-08-2021
+ *Author Name:Shruti Nahar
+ *////////////////////////
+class NegativeRadiusException extends Exception{
+    @Override
+    public String toString() {
+        return "Radius cannot be negative!";
+    }
+
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative!";
+    }
+}
+
+public class throw_throws {
+
+    public static double area(int r) throws NegativeRadiusException{
+        if (r<0){
+            throw new NegativeRadiusException();
+        }
+        double result = Math.PI * r * r;
+        return result;
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException{
+        int result = a/b;
+        return result;
+    }
+    public static void main(String[] args) {
+       
+        try{
+            int c = divide(6, 0);
+            System.out.println(c);
+            double ar = area(6);
+            System.out.println(ar);
+        }
+        catch(Exception e){
+            System.out.println("Exception");
+        }
+    }
+}
