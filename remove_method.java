9377a85bc774623e33e8a31ab8eb57dd61b51e9a
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-08-16 09:37:00 +0530

    Implementation of remove method in hash set class

--- remove_method.java
+++ remove_method.java
@@ -1,28 +1,26 @@
-/////////////////////////////
+////////////////////////////
 /*class:remove_method
 *Input:--
-*Output: Array list before : [1, 2, 3, 4, 6, 5]
-         ArrayList after removing the value at index 0 :[2, 3, 4, 6, 5]
-*Description:Implementation of remove methods in Array list class
-*Date:13-08-2021
+*Output:myHashSet before removing any element : [8, 3, 11, 6]
+        myHashSet after removing a element : [8, 11, 6]
+*Description:Implementation of remove method in hash set class
+*Date:16-08-2021
 *Author Name:Shruti Nahar
 *///////////////////////////////////
+
 import java.util.*;
 public class remove_method extends Thread{
     public static void main(String[] args) {
+        HashSet<Integer> myHashSet = new HashSet<>(6, 0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(3);
+        myHashSet.add(11);
+        myHashSet.add(11); // This element will be ignored
 
-        ArrayList<Integer> l1 = new ArrayList<>();
-        l1.add(1);
-        l1.add(2);
-        l1.add(3);
-        l1.add(4);
-        l1.add(6);
-        l1.add(5,5);  // inserts 5 at the 5th index in l1
-
-        System.out.println("Array list before : "+ l1);
-
-        l1.remove(0);
-        System.out.println("ArrayList after removing the value at index 0 :" + l1);
+        System.out.println("myHashSet before removing any element : " + myHashSet);
+        myHashSet.remove(3); //deletes 3 from the hashset
+        System.out.println("myHashSet after removing a element : " + myHashSet);
 
     }
 }
