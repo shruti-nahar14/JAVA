e5b0a24badf1f7abe99969837df653b73141ff61
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-08-16 09:34:08 +0530

    Implementation of hash set class in collection interface

--- hashset/src/hashset/bookhash.java
+++ hashset/src/hashset/bookhash.java
@@ -1,3 +1,16 @@
+////////////////////////////
+/*class:bookhash
+*Input: 103	java	500
+        102	oracle	600
+        101 cpp     800
+        101 cpp     800
+*Output:101	cpp  	800
+        102	oracle	600
+        103	java	500    
+*Description:Implementation of hash set class in collection interface
+*Date:16-08-2021
+*Author Name:Shruti Nahar
+*///////////////////////////////////
 package hashset;
 
 import java.util.Set;
