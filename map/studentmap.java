f1b7b02cdc889bcbc7dcb3ef4b7789794c93b2a3
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-08-18 09:05:48 +0530

    Implementation of hashmap class in collection interface

--- map/studentmap.java
+++ map/studentmap.java
@@ -1,3 +1,42 @@
+////////////////////////////
+/*class:studentmap
+*Input:
+-----------------------------STUDENT DETAILS---------------------------------------
+Enter the Studentid 
+101
+Enter the Student name 
+shruti
+Enter the math marks 
+76
+Enter the Science marks 
+87
+Enter the Socialscience marks 
+56
+Do you want add details press 1 
+1
+-----------------------------STUDENT DETAILS---------------------------------------
+Enter the Studentid 
+102
+Enter the Student name 
+shubham
+Enter the math marks 
+65
+Enter the Science marks 
+98
+Enter the Socialscience marks 
+76
+Do you want add details press 1 
+2
+
+*Output:
+ID	STUDENTNAME	SUB1	SUB2	SUB3	TOTAL	PERCENTAGE
+101	 shruti		76	    87	     56	    219	     73.0
+102	 shubham    65	    98	     76	    239	     79.0
+ 
+*Description:Implementation of hashmap class in collection interface
+*Date:18-08-2021
+*Author Name:Shruti Nahar
+*///////////////////////////////////
 package map;
 
 import java.util.Map;
