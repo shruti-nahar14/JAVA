0749f8f1410004bd1cdb76e495800c4dee2d4d75
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-20 08:35:32 +0530

    Implementation of date and time using inbuilt function

---
+++ date_time.java
@@ -0,0 +1,15 @@
+////////////////////////////
+/*class: date_time
+*Input:--
+*Output:year passed 51
+*Description:Implementation of date and time using inbuilt function
+*Date:19-08-2021
+*Author Name:Shruti Nahar
+*///////////////////////////////////
+package a;
+public class date_time {
+    public static void main(String[] args) {
+    	
+        System.out.println("year passed "+System.currentTimeMillis()/1000/3600/24/365);
+    }
+}
