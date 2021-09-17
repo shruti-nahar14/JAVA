7caaa0219f3cb0480c4be3fe4d72cd5097002d21
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-17 11:20:32 +0530

    Add files via upload

---
+++ Que1.java
@@ -0,0 +1,25 @@
+/*Write a program to print "Good morning" and "Welcome" 
+ * continuously on the screen in Java using threads.*/
+
+class Thread1 extends Thread{
+    public void run(){
+        while (true){
+        System.out.println("Welcome");
+    }
+    }
+}
+class Thread2 extends Thread {
+    public void run() {
+        while (true) {
+            System.out.println("Good morning");
+        }
+    }
+}
+public class Que1 {
+    public static void main(String[] args) {
+        Thread1 t1= new Thread1();
+        Thread2 t2= new Thread2();
+        t1.start();
+        t2.start();
+    }
+}
